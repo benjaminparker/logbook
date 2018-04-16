5039fe77f9665ff1d7e69698abac87815d105904
Benjamin Parker <benjamin@needstyping.com>
2018-04-16 11:15:15 +0100

    Wiring up reactive mongo and persisting a dive with one field populated

--- app/controllers/LogbookController.scala
+++ app/controllers/LogbookController.scala
@@ -2,11 +2,18 @@ package controllers
 
 import com.google.inject.Inject
 import models.Dive
+import models.JsonFormats._
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.json._
 import play.api.mvc._
+import play.modules.reactivemongo._
+import reactivemongo.play.json.collection.JSONCollection
 
-class LogbookController @Inject() (val cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+import scala.concurrent.{ExecutionContext, Future}
+
+class LogbookController @Inject() (messagesAction: MessagesActionBuilder, mcc: MessagesControllerComponents, val reactiveMongoApi: ReactiveMongoApi) extends AbstractController(mcc)
+  with MongoController with ReactiveMongoComponents {
 
   private val logbookForm: Form[Dive] = Form(
     mapping(
@@ -22,18 +29,25 @@ class LogbookController @Inject() (val cc: MessagesControllerComponents) extends
     )(Dive.apply)(Dive.unapply)
   )
 
-  def show = Action {
-    implicit request =>
+  implicit def ec: ExecutionContext = mcc.executionContext
+
+  def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("dives"))
+  
+  def show = messagesAction { implicit request: MessagesRequest[AnyContent] =>
       Ok(views.html.addDive(logbookForm))
   }
 
-  def add = Action {
-    implicit request =>
+  def add = messagesAction { implicit request: MessagesRequest[AnyContent] =>
       logbookForm.bindFromRequest().fold(
-        formWithErrors => BadRequest(views.html.addDive(formWithErrors.withGlobalError("Invalid dive"))),
+        
+        formWithErrors => {
+          BadRequest(views.html.addDive(formWithErrors.withGlobalError("Invalid dive")))
+        },
 
         dive => {
-          Ok("All good")
+            collection.flatMap(_.insert(dive))    //need to return this future result and use it when I understand this messagesAction malarky
+//          futureResult.map(_ => Ok("Dive added successfully"))
+          Ok("Dive added successfully")
         }
       )
   }
--- app/models/Dive.scala
+++ app/models/Dive.scala
@@ -7,3 +7,10 @@ case class Dive(when: LocalDateTime = LocalDateTime.now, diveShop: String = "", 
   require(bottomTime >= 5)
   require(oxygenPercentage >= 21 && oxygenPercentage <= 100)
 }
+
+object JsonFormats {
+  import play.api.libs.json.Json
+
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+  implicit val diveFormat = Json.format[Dive]
+}
